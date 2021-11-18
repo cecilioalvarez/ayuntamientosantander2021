5fcbb13b4307247505962944a0085ddb36797694
cecilioAlvarez <cmac@arquitecturajava.com>
2021-11-18 09:31:40 +0100

    compoicion

--- 008net/Ordenador.cs
+++ 008net/Ordenador.cs
@@ -6,8 +6,17 @@ namespace _008net
 
         public string Marca { get; set; }
         public int Precio { get; set; }
+
+        public Monitor MonitorA {get;set;}
         
-        
+        public Ordenador(string Marca, int Precio,  Monitor MonitorA) {
+
+                this.Marca=Marca;
+                this.Precio=Precio;
+                this.MonitorA=MonitorA;
+                
+        }
+       
         
     }
 
--- 008net/Program.cs
+++ 008net/Program.cs
@@ -6,7 +6,15 @@ namespace _008net
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Monitor m1= new Monitor("samsung",30);
+           Ordenador o= new Ordenador("dell",1000,m1);
+           Console.WriteLine(o.Marca);
+           Console.WriteLine(o.MonitorA.Marca);
+           Monitor m2= new Monitor("lg",24);
+           o.MonitorA=m2;
+           Console.WriteLine(o.Marca);
+           Console.WriteLine(o.MonitorA.Marca);
+           
         }
     }
 }
