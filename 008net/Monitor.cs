ee2d33f75becdf7d686ccbef92ccc800b14fd83e
cecilioAlvarez <cmac@arquitecturajava.com>
2021-11-18 09:21:16 +0100

    nuevos

---
+++ 008net/Monitor.cs
@@ -0,0 +1,16 @@
+namespace _008net
+{
+
+public class Monitor {
+
+    public string Marca {get;set;}
+    public int Pulgadas {get;set;}
+
+    public Monitor(string Marca, int Pulgadas)
+    {
+        this.Marca=Marca;
+        this.Pulgadas=Pulgadas;
+    }
+}
+
+}
