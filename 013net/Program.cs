3427be5b091ddfad1baf8f79a2cfc9828a229678
cecilioAlvarez <cmac@arquitecturajava.com>
2021-11-19 13:58:12 +0100

    nuevos ficheros

---
+++ 013net/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+namespace _013net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string fichero = @"facturas.txt";
+ 
+        string[] lineas = File.ReadAllLines(fichero);
+
+        for (int i=0;i<lineas.Length;i++) {
+
+            string[] propiedad=lineas[i].Split(",");
+            /*
+            for (int j=0;j<propiedad.Length;j++) {
+
+                Console.WriteLine(propiedad[j]);
+            }*/
+            int numero= Convert.ToInt32(propiedad[0]);
+            string concepto=propiedad[1];
+            Factura factura= new Factura();
+        }
+        }
+    }
+}
